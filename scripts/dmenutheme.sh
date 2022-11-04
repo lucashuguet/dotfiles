ac2692ca5f445432a90db9f8ae44670a54e3b36f
Lucas Huguet <lucashuguet31@gmail.com>
2022-11-04 05:04:42 +0100

    update theme scripts

--- scripts/dmenutheme.sh
+++ scripts/dmenutheme.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
 file=$(ls ~/Pictures/wallpapers/ | sed "s/.png$//g" | dmenu -i)
-if [ $? == 1 ]
-then
-	xrdb -remove
-	feh --bg-fill ~/dotfiles/wallpaper/gobacktothefuture.png
-	killall autostart.sh
-	xdotool key super+r
-	exit
+if [ $? == 1 ]; then
+	exit 0
+else
+	if [ "$file" = "default" ]; then
+		xrdb -remove
+		feh --bg-fill ~/dotfiles/wallpaper/gobacktothefuture.png
+		killall autostart.sh
+		xdotool key super+r
+		exit 0
+	else
+		echo $file
+		settheme.sh "/home/astrogoat/Pictures/wallpapers/$file.png"
+	fi
 fi
 
-settheme.sh "/home/astrogoat/Pictures/wallpapers/$file.png"
--- scripts/getlightcolor.py
+++ scripts/getlightcolor.py
@@ -5,6 +5,12 @@ import sys
 def clamp(n, smallest, largest): 
     return max(smallest, min(n, largest))
 
+def twolen(hexa):
+    if len(hexa) == 1:
+        return "0" + hexa
+    else:
+        return hexa
+
 color = sys.argv[1]
 
 if not len(color) == 7:
@@ -17,4 +23,4 @@ b = color[5:7]
 rgb = (int(r, base=16), int(g, base=16), int(b, base=16))
 light = (clamp(int(rgb[0] * 1.2), 0, 255), clamp(int(rgb[1] * 1.2), 0, 255), clamp(int(rgb[2] * 1.2), 0, 255))
 
-print("#" + str(hex(light[0]))[2:] + str(hex(light[1]))[2:] + str(hex(light[2]))[2:])
+print("#" + twolen(str(hex(light[0]))[2:]) + twolen(str(hex(light[1]))[2:]) + twolen(str(hex(light[2]))[2:]))
