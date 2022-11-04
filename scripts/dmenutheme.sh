0100e9d3afec34f94f676da0696b743452bd57b2
Lucas Huguet <lucashuguet31@gmail.com>
2022-11-04 04:29:36 +0100

    enable insensitive option

--- scripts/dmenutheme.sh
+++ scripts/dmenutheme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-file=$(ls ~/Pictures/wallpapers/ | sed "s/.png$//g" | dmenu)
+file=$(ls ~/Pictures/wallpapers/ | sed "s/.png$//g" | dmenu -i)
 if [ $? == 1 ]
 then
 	xrdb -remove
