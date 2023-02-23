e2996ce4fef2baa9d5ffd58e0c07051aad80c60f
Lucas Huguet <lucashuguet31@gmail.com>
2023-02-23 20:05:09 +0100

    set gtk theme on wayland + forgot delete hyprpaper config

---
+++ scripts/gtktheme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme Sweet-Dark
+gsettings set org.gnome.desktop.interface icon-theme Tela-blue-dark
+gsettings set org.gnome.desktop.interface cursor-theme Sweet-cursors
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
